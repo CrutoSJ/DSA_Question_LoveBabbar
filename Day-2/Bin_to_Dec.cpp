07e2befe0498e34e7bb557ec430509860ca330f3
Sumit <jha.07sumit2003@gmail.com>
2023-12-03 22:33:40 +0530

    Added 3 Bin_to_Dec Que and vice versa, 1 Que from Leetcode i.e .cpp

---
+++ Day-2/Bin_to_Dec.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+
+        int n;
+        cin >> n;
+        int i=0;
+        int ans =0;
+
+        while(n!=0){
+
+            int digit = n%10;
+
+            if(digit==1){
+                ans = ans+ pow(2,i);
+
+            }
+
+            i++;
+            n = n/10;
+        }
+
+        cout << ans;
+
+    }
+}
