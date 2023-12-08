9408d463562de7771306269e7fc595a9ac30b8c8
Sumit <jha.07sumit2003@gmail.com>
2023-12-08 14:33:07 +0530

    Added 1207.cpp and its approach

---
+++ Day-6/1207.cpp
@@ -0,0 +1,32 @@
+// #Question:-
+
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int i = 0;
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        while (i < arr.size()){
+            int count = 1; //evry no. comes atleast once.
+            for (int j = i+1; j< arr.size(); j++){
+                if (arr[i] == arr[j])
+                    count++;
+            }
+            ans.push_back(count);
+            i = i + count; //skipping the same elements, jidhar start kiya+same nos. 
+        }
+        sort(ans.begin(),ans.end()); //so any same occurance come next to each other.
+        for (int i = 0; i < ans.size() - 1; i++){
+            if (ans[i] == ans [i+1])
+                return false;
+        }
+        return true;
+    }
+};
