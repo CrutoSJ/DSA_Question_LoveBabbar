5ca1da9801ea094b3fbea7db761e768a2047f2ab
Sumit <jha.07sumit2003@gmail.com>
2023-12-18 19:09:30 +0530

    Added Bubble Sort - O(n^2) [worst case] | O(n) [best case]

---
+++ Day-16/BubbleSort.cpp
@@ -0,0 +1,25 @@
+// #Question:-
+
+// https://www.codingninjas.com/studio/problems/bubble-sort_980524
+
+// #Solution:-
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i=1; i<n; i++){
+        bool swapped = false;
+
+        for( int j=0; j<n-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(swapped == false){
+            break;
+        }
+    }
+}
