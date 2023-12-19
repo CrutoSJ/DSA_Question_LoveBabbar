4fff80a607242a610a6f4b6659b2dcbe42c920a8
Sumit <jha.07sumit2003@gmail.com>
2023-12-19 20:11:53 +0530

    Added Insertion sort

---
+++ Day-17/InsertionSort.cpp
@@ -0,0 +1,27 @@
+// #Question:-
+
+// https://www.codingninjas.com/studio/problems/insertion-sort_3155179
+
+// #Solution:-
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j=i-1;
+        for(; j>=0; j--){
+
+            if(arr[j]>temp){
+                arr[j+1]=arr[j];
+            }
+
+            else{
+                break;
+            }
+        }
+
+        arr[j+1]=temp;
+    }
+}
