4e62629a5945415b6eebf20f38475b4438c4601c
Sumit <jha.07sumit2003@gmail.com>
2023-12-21 20:55:51 +0530

    Added 540 - simple for loop using XOR Operation

---
+++ Day-19/540.cpp
@@ -0,0 +1,22 @@
+// #Question:-
+
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+// Return the single element that appears only once.
+
+// Your solution must run in O(log n) time and O(1) space.
+
+// #Solution:-
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int sum=0;
+        for(int i=0; i<nums.size(); i++){
+            sum = sum^nums[i];
+        }
+        
+        return sum;
+        
+    }
+};
