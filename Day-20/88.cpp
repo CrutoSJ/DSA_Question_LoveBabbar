addcdd06c76aa6736b41d0e1f9713d5d4f0874a2
Sumit <jha.07sumit2003@gmail.com>
2023-12-22 20:19:06 +0530

    Added 88 of leetcode - using two pointer for swap then using sort from c++ STL

---
+++ Day-20/88.cpp
@@ -0,0 +1,31 @@
+// #Question:-
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0;
+        int j=0;
+        
+        while(i<(m+n) && j<n){
+            if(nums1[i]==0){
+                swap(nums1[i], nums2[j]);
+                j++;
+            }
+            
+            i++;
+        }
+        
+        sort(nums1.begin(), nums1.end());
+    }
+};
