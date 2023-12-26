7261c64c6a226664e740c110e233ea915551f76f
Sumit <jha.07sumit2003@gmail.com>
2023-12-26 17:36:29 +0530

    Added 2D Array 1st que - simple for loop logic with if statement

---
+++ Day-24/PrintLikeWave.cpp
@@ -0,0 +1,31 @@
+// #Question:-
+
+// https://www.codingninjas.com/studio/problems/print-like-a-wave_893268
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+
+    for(int col = 0; col<mCols; col++){
+
+        if(col%2==0){
+            for(int row=0; row<nRows; row++){
+                ans.push_back(arr[row][col]);
+            }
+        }
+
+        else{
+            for(int row=nRows-1; row>=0; row--){
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+
+    return ans;
+}
