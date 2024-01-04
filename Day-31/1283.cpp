cc1d5493290b094f6cdcc1fb0d2daa89e771e6cd
Sumit <jha.07sumit2003@gmail.com>
2024-01-04 23:28:34 +0530

    Did 1283 - Bin Search, Optimized and added the corrected solution

--- Day-31/1283.cpp
+++ Day-31/1283.cpp
@@ -10,17 +10,15 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-#include<bits/stdc++.h>
 class Solution {
     
 private: 
-    bool isSmallest(vector<int>& nums, int mid, int threshold){
-        int sum=0;
+    bool isSmallest(int mid, int threshold, vector<int>& nums){
+        double sum=0;
         int n = nums.size();
         
-        for(int i = 0; i < n; i++){
-            sum += ceil(static_cast<double>(nums[i]) / nums[mid]);
+        for(int i = 0; i <n; i++){
+            sum += ceil(static_cast<double>(nums[i]) / mid);
         }
         
         if(sum<=threshold){
@@ -34,18 +32,16 @@ private:
     
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int s = 0;
-        int e = nums.size()-1;
-        
-        sort(nums.begin(), nums.end());
+        int s = 1;
+        int e = *max_element(nums.begin(), nums.end());
         int ans = -1;
         
         while(s<=e){
             
             int mid = s + (e-s)/2;
             
-            if(isSmallest(nums, mid, threshold)){
-                ans = nums[mid];
+            if(isSmallest(mid, threshold, nums)){
+                ans = mid;
                 e = mid-1;
             }
             
