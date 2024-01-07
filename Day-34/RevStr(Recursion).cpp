416a24a0e070ce0cce3d758b19f2bd729d16f1a5
Sumit <jha.07sumit2003@gmail.com>
2024-01-07 15:36:31 +0530

    Did this question with recursion - and also updated solution by removing 1 extra pointer j

--- Day-34/RevStr(Recursion).cpp
+++ Day-34/RevStr(Recursion).cpp
@@ -7,16 +7,19 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-void revString(string& str, int i, int j){
-	if(i>j){
+void revString(string& str, int i){
+	int n = str.length();
+	if(i>n-i-1){
 		return ;
 	}
-	swap(str[i], str[j]);
-	revString(str, i+1, j-1);
+
+	swap(str[i], str[n-i-1]);
+
+	revString(str, i+1);
 }
 
 string reverseString(string str)
 {
-	revString(str, 0, str.length()-1);
+	revString(str, 0);
 	return str;
 }
