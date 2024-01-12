3c141604fbec98959fb5306023abcc6ce8265ab6
Sumit <jha.07sumit2003@gmail.com>
2024-01-12 16:06:52 +0530

    Another question using Recursion & Backtracking

---
+++ Day-38/46.cpp
@@ -0,0 +1,31 @@
+// #Question:-
+
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    void solve(vector<int> nums, vector<vector<int>>& ans, int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+        
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[i], nums[index]);
+            solve(nums, ans, index+1);
+            swap(nums[i], nums[index]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
