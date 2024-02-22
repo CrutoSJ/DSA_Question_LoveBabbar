fdf428871b0fe373dc0aaa0d4d38736f08cfcbb5
Sumit <jha.07sumit2003@gmail.com>
2024-02-22 14:43:27 +0530

    Did this withouth using Stack to remove using extra space

---
+++ Day-80/1021.cpp
@@ -0,0 +1,38 @@
+// #Question:-
+
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+// Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+// Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' && count==0)
+                count++;
+            else if(s[i]=='(' && count>=1){
+                res+=s[i];
+                count++;
+            }   
+            else if(s[i]==')' && count>1){
+                res+=s[i];
+                count--;
+            }
+            else if(s[i]==')' && count==1)
+                count--;
+        }
+        return res;
+    }
+};
