93f7879356e6dfbaed2ca1c222f31d087f0c34c3
Sumit <jha.07sumit2003@gmail.com>
2024-03-04 22:27:54 +0530

    Did an easy level question, just for revision - by travesing the LL

---
+++ Day-91/SearchInLL.cpp
@@ -0,0 +1,43 @@
+// #Question:-
+
+// Link-> https://www.codingninjas.com/studio/problems/search-in-a-linked-list_975381
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+    class Node
+    {
+    public:
+        T data;
+        Node<T> *next;
+        Node()
+        {
+            this->data = 0;
+            this->next = NULL;
+        }
+        Node(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(T data, T* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+int searchInLinkedList(Node<int> *head, int k) {
+    Node<int> *temp = head;
+    while(temp != NULL){
+        if(temp->data == k){
+            return 1;
+        }
+        temp = temp->next;
+    }
+
+    return 0;
+}
