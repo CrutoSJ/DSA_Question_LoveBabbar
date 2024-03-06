e73b5d08691d949a5ff25db946ff96e47cfecd30
Sumit <jha.07sumit2003@gmail.com>
2024-03-06 10:43:26 +0530

    Did this question by simply creating a node then pointing it to head, then changing the head

---
+++ Day-92/InsertAtLL.cpp
@@ -0,0 +1,25 @@
+// #Question:-
+
+// Link-> https://www.codingninjas.com/studio/problems/insert-node-at-the-beginning_8144739
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    
+    public:
+        int data;
+        Node* next;
+        Node() : data(0), next(nullptr) {}
+        Node(int x) : data(x), next(nullptr) {}
+        Node(int x, Node* next) : data(x), next(next) {}
+    };
+
+    Node* insertAtFirst(Node* list, int newValue) {
+    Node* temp = new Node(newValue);
+    temp->next = list;
+    Node* head = temp;
+    return head;
+}
