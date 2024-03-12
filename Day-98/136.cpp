92ee9cd8c1f5b293d8a21202d76659e642afef79
Sumit <jha.07sumit2003@gmail.com>
2024-03-12 21:44:42 +0530

    Did this simply by using Logic Gates

---
+++ Day-98/136.cpp
@@ -0,0 +1,23 @@
+// #Question:-
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans ^= nums[i];
+        }
+        
+        return ans;
+    }
+};
