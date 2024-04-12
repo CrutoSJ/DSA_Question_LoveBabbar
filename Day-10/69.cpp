ec5d4dbd0e88e8125de65b46ea1a5b2bf80c579e
Sumit <jha.07sumit2003@gmail.com>
2024-04-12 10:56:01 +0530

    Did this question with Bin Search, storing in ans variable

--- Day-10/69.cpp
+++ Day-10/69.cpp
@@ -1,4 +1,15 @@
-// 
+//Questions:-
+
+// //Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+// Link -> https://leetcode.com/problems/sqrtx/
+
+//Attempt - 02
+
 #include<bits/stdc++.h>
 using namespace std;
 
