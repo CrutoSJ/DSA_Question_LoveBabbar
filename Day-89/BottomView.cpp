c83c2b37a39eca968dd43b6cb89a631afd2a68d8
Sumit <jha.07sumit2003@gmail.com>
2024-06-14 19:00:33 +0530

    Did this with the same logic of top view, just remove the condition of input only 1 time

---
+++ Day-89/BottomView.cpp
@@ -0,0 +1,53 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class Solution {
+public:
+    vector <int> bottomView(Node *root) {
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        
+        map<int,int>nodes;
+        queue<pair<Node*,int> >q;
+        
+        q.push(make_pair(root,0));
+        
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            
+            Node* frontNode = temp.first;
+            int hd = temp.second;
+            
+            nodes[hd] = (frontNode->data);
+            
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+        }
+        for(auto i:nodes){
+            ans.push_back(i.second);
+        }
+        
+        return ans;
+    }
+};
